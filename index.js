27e53ca8a417694638be48f3e0891d5b7e4150e2
Chandan Kumar Saw <chandan1951c@gmail.com>
2023-08-19 03:54:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ function get(){
     var x = document.getElementById("demo");
 
     var text = " ";
-    for(let j=1;j<=y;j++)
-    {
-        text+="<br><br>Table of "+j+" is <br><br>"
+    //for(let j=1;j<=y;j++)
+  //  {
+        text+="<br><br>Table of "+y+" is <br><br>"
         for(let i=1;i<=10;i++)
         {
-            text+= + j +" X "+i+" = " + j*i + "<br>"
+            text+= + y +" X "+i+" = " + y*i + "<br>"
         }
-    }
+  //  }
 
     x.innerHTML = text;
     
